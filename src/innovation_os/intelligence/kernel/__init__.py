60d66b10d3395925458f8ce47b71ced151428046
William King <wking53214@gmail.com>
2026-07-28 06:08:33 -0400

    Restore cognitive kernel and registry exports

--- src/innovation_os/intelligence/kernel/__init__.py
+++ src/innovation_os/intelligence/kernel/__init__.py
@@ -1,7 +1,26 @@
-from .cognitive_kernel import CognitiveKernel
-from .intelligence_registry import IntelligenceRegistry
+from .cognitive_kernel import (
+    CognitiveKernel,
+)
+
+from .intelligence_registry import (
+    IntelligenceRegistry,
+)
+
+from .kernel_runtime import (
+    IntelligenceKernel,
+)
+
+
+
+def create_kernel():
+
+    return IntelligenceKernel()
+
+
 
 __all__ = [
     "CognitiveKernel",
     "IntelligenceRegistry",
+    "IntelligenceKernel",
+    "create_kernel",
 ]
